1086de7354ce2b6550d84616276341ca25f33d18
Damilare <blankgodd33@gmail.com>
2020-10-24 05:24:09 +0100

    dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 const menuBtn = document.querySelector(".menu-btn");
-const hamburger = document.querySelector("menu-btn__burger");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
 
 let showNav = false;
 
 menuBtn.addEventListener('click', addOpen);
 
 function addOpen() {
-    if(!showNav){
+    if(!showNav) {
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
 
         showNav = true;
     } else {
         hamburger.classList.remove("open");
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
 
-        showNav = false;
+        showNav = false
     }
 }
+
 
